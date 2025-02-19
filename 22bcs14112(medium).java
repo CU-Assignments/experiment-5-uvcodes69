9ede77a0b1f230ac2eb9c4833846c6411534ed97
uvcodes69 <154894120+uvcodes69@users.noreply.github.com>
2025-02-19 14:30:38 +0530

    Create 22bcs14112(medium).java

---
+++ 22bcs14112(medium).java
@@ -0,0 +1,46 @@
+import java.io.*;
+
+class Student implements Serializable {
+    private int id;
+    private String name;
+    private double gpa;
+
+    public Student(int id, String name, double gpa) {
+        this.id = id;
+        this.name = name;
+        this.gpa = gpa;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{id=" + id + ", name='" + name + "', gpa=" + gpa + "}";
+    }
+}
+
+public class SerializationExample {
+    public static void main(String[] args) {
+        Student student = new Student(1, "John Doe", 3.75);
+
+        try {
+            FileOutputStream fileOut = new FileOutputStream("student.ser");
+            ObjectOutputStream out = new ObjectOutputStream(fileOut);
+            out.writeObject(student);
+            out.close();
+            fileOut.close();
+            System.out.println("Student object serialized");
+
+            FileInputStream fileIn = new FileInputStream("student.ser");
+            ObjectInputStream in = new ObjectInputStream(fileIn);
+            Student deserializedStudent = (Student) in.readObject();
+            in.close();
+            fileIn.close();
+            System.out.println("Deserialized Student: " + deserializedStudent);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+}
